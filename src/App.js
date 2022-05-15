a0ddd3b2c6c4d3f4c2f8036988853b774bad4c2d
Shriprasanna B <shriprasanna32@gmail.com>
2022-05-15 15:18:09 +0530

    comment6s

--- src/App.js
+++ src/App.js
@@ -20,3 +20,15 @@ function App() {
 }
 
 export default App;
+
+// BrowserRouter -> is basically a div like thing we add the routing
+//                 components in it to route to the particular page
+
+//        Routes -> is nothing but when we specify the route path we need
+//                 use it a paired tag
+
+//          Route -> by using Route we set path to the components using
+//                  "path"  attribute and path to navigate to the componet as
+//                  "element" attribute
+
+//path="*" => is random wrong stuff eg;- /sasasa it route top error page
--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -9,7 +9,7 @@ export default function Error() {
   return (
     <>
       <h1 className="text-6xl text-center mt-10 bg-sky-100 h-[90px] text-slate-600">
-        Error page
+        Error page 404
       </h1>
       <button
         onClick={pageRedirect}
