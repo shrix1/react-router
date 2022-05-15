800936b443ee2babc4e3c21857a5219cec6367f0
Shriprasanna B <shriprasanna32@gmail.com>
2022-05-15 15:43:47 +0530

    css changes

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -10,12 +10,16 @@ export default function Error() {
 
   return (
     <>
-      <h1 className="text-6xl text-center mt-10 bg-sky-100 h-[90px] text-slate-600">
+      <h1
+        className="text-6xl text-center mt-10 bg-sky-100
+        h-[90px] text-slate-600"
+      >
         Error page 404
       </h1>
       <button
         onClick={pageRedirect}
-        className="border-4 border-slate-800 bg-sky-400 ml-[850px] mt-6 p-4"
+        className="border-4 border-slate-800 bg-sky-400 ml-[850px]
+         mt-6 p-4 rounded-md"
       >
         go to main page
       </button>
