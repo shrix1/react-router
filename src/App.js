fe4959279f461e2574ca065b30b4dab0d44abee7
Shriprasanna B <shriprasanna32@gmail.com>
2022-05-15 14:33:57 +0530

    mioan]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Nav from "./components/Nav";
 import About from "./components/About";
 import Store from "./components/Store";
+import { BrowerRouter ,Route , Switch } form "react-router-dom";
 
 function App() {
   return (
